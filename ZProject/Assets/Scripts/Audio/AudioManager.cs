a9cb96e7197fc21293d93218a38cb1320b4ca8ec
Az <thomas.guyamier@gmail.com>
2020-11-18 11:47:42 +0100

    Ajout Sound manager

---
+++ ZProject/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+    public AudioMixerGroup mainMixer;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+            s.source.outputAudioMixerGroup = s.mixer;
+        }
+    }
+
+    private void Start()
+    {
+        Play("Theme01");
+    }
+
+    public void Play(string name)
+    {
+       Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found");
+            return;
+        }
+        s.source.Play(0);
+    }
+
+    public enum volumeType
+    {
+        master,
+        music,
+        sound
+    }
+}
