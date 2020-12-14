2bd29c7f25ea992769affcd47238d87b05d355b4
Gen0902 <50872015+Gen0902@users.noreply.github.com>
2020-12-14 09:58:54 +0100

    startWeapon system

---
+++ ZProject/Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour
+{
+	FireWeaponData data;
+    public float actualDamages;
+    public int loaderData;
+
+    public PlayerWeapon()
+	{
+		data.loaderCapacity = data.loaderMaxCapacity;
+
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ZProject/Assets/Scripts/WeaponData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "WeaponData", menuName = "Weapons", order = 1)]
+public class FireWeaponData : ScriptableObject
+{
+    public string name = "Glock";
+    public float baseDamages;
+    public float upgradeFactor;
+    public int level = 1;
+
+    public int cost;
+    public int loaderCapacity;
+    public int loaderMaxCapacity;
+    public int maxCapacity;
+    public float reloadTime = 1f;
+    public float fireRate = 0f;
+    public GameObject graphics;
+
+
+    public GameObject mesh;
+
+}
+
+public enum EWeaponType { Pistol, Shotgun, Smg, Sniper, MachineGun}
